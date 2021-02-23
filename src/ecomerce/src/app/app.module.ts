05852531f0d56a3f2229a725a07637d56e27847a
jamal k <myjamal89@gmail.com>
2021-02-23 19:10:48 +0100

    shiping form

--- src/ecomerce/src/app/app.module.ts
+++ src/ecomerce/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { AdressTestComponent } from './adress-test/adress-test.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -21,7 +25,8 @@ import {MatSelectModule} from '@angular/material/select';
     CounterComponent,
     ClockComponent,
     Clock2Component,
-    ContactFormComponent
+    ContactFormComponent,
+    AdressTestComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +36,10 @@ import {MatSelectModule} from '@angular/material/select';
     MatSliderModule,
     MatButtonModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
