a556e6510dac94cce2303f28ade2122110ed43c9
jamal k <myjamal89@gmail.com>
2021-02-25 20:06:07 +0100

    improve adresse form

--- src/ecomerce/src/app/adress-test/adress-test.component.ts
+++ src/ecomerce/src/app/adress-test/adress-test.component.ts
@@ -8,9 +8,11 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class AdressTestComponent {
   addressForm = this.fb.group({
-    company: null,
     firstName: [null, Validators.required],
     lastName: [null, Validators.required],
+    productType: [null, Validators.required],
+    productLink: [null, Validators.required],
+    company: null,
     address: [null, Validators.required],
     address2: null,
     city: [null, Validators.required],
@@ -24,70 +26,24 @@ export class AdressTestComponent {
   hasUnitNumber = false;
 
   states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
+    { name: 'Agadir', abbreviation: 'AL' },
+    { name: 'Rabat', abbreviation: 'AK' },
+    { name: 'Casablanca', abbreviation: 'AS' },
+    { name: 'Titouane', abbreviation: 'AZ' },
+    { name: 'Fes', abbreviation: 'AR' },
+    { name: 'Wejda', abbreviation: 'CA' },
   ];
 
-  constructor(private fb: FormBuilder) {}
+
+  productsTypes = [
+    { name: 'mode' },
+    { name: 'electronic' },
+    { name: 'food' }
+  ];
+  constructor(private fb: FormBuilder) { }
 
   onSubmit() {
-    alert('Thanks!');
+    
+    alert(JSON.stringify(this.addressForm.value));
   }
 }
--- src/ecomerce/src/app/app.module.ts
+++ src/ecomerce/src/app/app.module.ts
@@ -5,10 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import { ClockComponent } from './clock/clock/clock.component';
-import { Clock2Component } from './clock2/clock2/clock2.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContactFormComponent } from './contact-form/contact-form/contact-form.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -23,9 +20,6 @@ import { MatCardModule } from '@angular/material/card';
   declarations: [
     AppComponent,
     CounterComponent,
-    ClockComponent,
-    Clock2Component,
-    ContactFormComponent,
     AdressTestComponent
   ],
   imports: [
@@ -43,6 +37,6 @@ import { MatCardModule } from '@angular/material/card';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [CounterComponent]
+  exports: []
 })
 export class AppModule { }
